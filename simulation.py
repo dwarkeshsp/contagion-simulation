cf1e38a2374134695e0bd19de44a90bea044a6a9
Dwarkesh Patel <dwarkesh.sanjay.patel@gmail.com>
2020-04-14 09:56:59 -0500

    gravity works!!!

--- simulation.py
+++ simulation.py
@@ -15,6 +15,8 @@ class Person(pygame.sprite.Sprite):
 people = []
 
 clock = pygame.time.Clock()
+CENTER = 500
+
 
 pygame.init()
 pygame.display.set_caption('Memetic Evolution Simulation')
@@ -25,13 +27,22 @@ def random_speed():
     return random.randint(0, 20) / 10 - 1
 
 
+def gravity(x):
+    gravity = (CENTER - x) ** 2 / 100000.0
+    if x > CENTER:
+        gravity *= -1
+    print(gravity)
+    return gravity
+
+
 running = True
+
 while running:
     screen.fill((0, 0, 0))
     # border = pygame.draw.rect(screen, (125, 125, 125),
     #                           (250, 250, 500, 500), 3)
 
-    people.append(Person(500, 500, random_speed(), random_speed()))
+    people.append(Person(CENTER, CENTER, random_speed(), random_speed()))
 
     for person in people:
         person.rect.x += person.dx
@@ -40,9 +51,8 @@ while running:
         #     person.dx *= - 1
         #     person.dy *= - 1
         # else:
-        person.dx += random_speed()
-        person.dy += random_speed()
-
+        person.dx += random_speed() + gravity(person.rect.x)
+        person.dy += random_speed() + gravity(person.rect.y)
         pygame.draw.rect(screen, (255, 255, 255),
                          person.rect)
 
