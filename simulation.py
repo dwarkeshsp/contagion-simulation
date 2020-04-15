17f23126db78c61ab580083a6254406090bea88d
Dwarkesh Patel <dwarkesh.sanjay.patel@gmail.com>
2020-04-15 12:11:54 -0500

    rect collision not changing the color

--- simulation.py
+++ simulation.py
@@ -4,61 +4,71 @@ import random
 
 
 class Person(pygame.sprite.Sprite):
-    def __init__(self, x, y, dx, dy):
+    def __init__(self, x, y, status):
         pygame.sprite.Sprite.__init__(self)
-        self.rect = pygame.Rect(0, 0, 6, 6)
+        self.rect = pygame.Rect(0, 0, 5, 5)
         self.rect.center = (x, y)
-        self.dx = dx
-        self.dy = dy
+        self.status = status
+        self.dx = 0
+        self.dy = 0
+
+    def move(self):
+        self.rect.x += self.dx
+        self.rect.y += self.dy
+
+        def random_speed():
+            return random.randint(0, 20) / 10 - 1
+
+        def gravity(x):
+            gravity = (CENTER - x) ** 2 / 5000.0
+            if x > CENTER:
+                gravity *= -1
+            return gravity
+
+        self.dx += random_speed() + gravity(self.rect.x)
+        self.dy += random_speed() + gravity(self.rect.y)
+
+        if self.status != 'sick':
+            for person in people:
+                if person.status == 'sick' and self.rect.colliderect(person.rect):
+                    print('detected')
+                    self.status == 'sick'
+
+        pygame.draw.rect(screen, self.color(), self.rect)
+
+    def color(self):
+        if self.status == 'healthy':
+            return (255, 255, 255)
+        if self.status == 'sick':
+            return (255, 255, 0)
+        else:
+            return None
 
 
-people = []
-
-clock = pygame.time.Clock()
 CENTER = 500
 
+people = []
+people.append(Person(CENTER, CENTER, status='sick'))
 
+clock = pygame.time.Clock()
 pygame.init()
 pygame.display.set_caption('Memetic Evolution Simulation')
 screen = pygame.display.set_mode((1000, 1000), 0, 32)
-
-
-def random_speed():
-    return random.randint(0, 20) / 10 - 1
-
-
-def gravity(x):
-    gravity = (CENTER - x) ** 2 / 100000.0
-    if x > CENTER:
-        gravity *= -1
-    print(gravity)
-    return gravity
 
 
 running = True
 
 while running:
     screen.fill((0, 0, 0))
-    # border = pygame.draw.rect(screen, (125, 125, 125),
-    #                           (250, 250, 500, 500), 3)
-
-    people.append(Person(CENTER, CENTER, random_speed(), random_speed()))
+    if len(people) < 100:
+        people.append(Person(CENTER, CENTER, status='healthy'))
 
     for person in people:
-        person.rect.x += person.dx
-        person.rect.y += person.dy
-        # if person.rect.x >= 750 or person.rect.x <= 250 or person.rect.y >= 750 or person.rect.y <= 250:
-        #     person.dx *= - 1
-        #     person.dy *= - 1
-        # else:
-        person.dx += random_speed() + gravity(person.rect.x)
-        person.dy += random_speed() + gravity(person.rect.y)
-        pygame.draw.rect(screen, (255, 255, 255),
-                         person.rect)
+        person.move()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(100)
